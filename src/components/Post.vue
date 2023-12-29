4c3afe3aa008572ae0006c6d0a8c38a6a19ceb91
uoohoon <hoonixox@naver.com>
2023-12-29 17:16:32 +0900

    feat: add like function

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -4,9 +4,9 @@
       <div class="profile" :style="`background-image:url(${게시물.userImage})`"></div>
       <span class="profile-name">{{ 게시물.name }}</span>
     </div>
-    <div :class="`${게시물.filter}`" class="post-body" :style="`background-image:url(${게시물.postImage})`"></div>
+    <div @click="$store.commit('getLike', this.게시물)" :class="`${게시물.filter}`" class="post-body" :style="`background-image:url(${게시물.postImage})`"></div>
     <div class="post-content">
-      <p>43 Likes</p>
+      <p>{{ 게시물.likes }} Likes</p>
       <p><strong>{{ 게시물.name }}</strong> {{ 게시물.content }}</p>
       <p class="date">{{ 게시물.date }}</p>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,11 +1,28 @@
 import { createStore } from 'vuex'
+import postData from './assets/postdata.js'
 
 const store = createStore({
   state(){
     return {
-      name : 'otter'
+      likes : 30,
+      likeOrNot : false,
+      postData: postData
     }
   },
-})
+  mutations: {
+  getLike(state, post) {
+    const index = state.postData.findIndex(item => item.id === post.id);
+    if (index !== -1) {
+      if (!state.postData[index].liked) {
+        state.postData[index].likes++;
+        state.postData[index].liked = true;
+      } else {
+        state.postData[index].likes--;
+        state.postData[index].liked = false;
+      }
+    }
+  }
+}
+});
 
 export default store
